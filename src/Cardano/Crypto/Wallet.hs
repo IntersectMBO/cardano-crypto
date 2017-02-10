28c11daa7bdd7958e584a2418a9b972f08ef45e2
Vincent Hanquez <vincent.hanquez@iohk.io>
2017-02-10 11:23:09 +0000

    [Wallet] Add derivation of keys

--- src/Cardano/Crypto/Wallet.hs
+++ src/Cardano/Crypto/Wallet.hs
@@ -1,7 +1,161 @@
+-- |
+-- Module      : Cardano.Crypto.Wallet
+-- Description : HD Wallet routines
+-- Maintainer  : vincent@typed.io
+--
+-- This provide similar functionality than BIP32 but using
+-- Ed25519 arithmetic instead of P256K1 arithmethic.
+--
+-- Key can be hierarchically derived from private key in two
+-- fashion: Hardened or Normal.
+--
+-- In the hardened scheme, the child secret key is not linearly
+-- derived, so that the child public key have no way
+-- to be efficiently computed from the parent public key.
+--
+-- The normal scheme, allows anyone to derive public keys from
+-- public key.
+--
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE BangPatterns #-}
 module Cardano.Crypto.Wallet
-    (
+    ( ChainCode(..)
+    -- * Extended Private & Public types
+    , XPrv
+    , XPub
+    , DerivationType(..)
+    , xprv
+    , xpub
+    , unXPrv
+    , unXPub
+    , toXPub
+    , xPubGetPublicKey
+    -- * Derivation function
+    , deriveXPrv
+    , deriveXPub
+    -- * Signature & Verification from extended types
+    , sign
+    , verify
     ) where
 
-import           Crypto.PubKey.Ed25519
+import           Crypto.OpenSSL.Random (randBytes)
+import qualified Crypto.PubKey.Ed25519 as Ed25519
 import qualified Crypto.ECC.Edwards25519 as Edwards25519
-import           Crypto.MAC.HMAC
+import           Crypto.Hash (SHA512)
+import qualified Crypto.MAC.HMAC as HMAC
+import           Crypto.Error (throwCryptoError)
+import           Data.Word
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as B (splitAt, length, append, pack)
+import           Data.ByteArray (ByteArrayAccess, convert)
+
+import Debug.Trace
+
+newtype ChainCode = ChainCode ByteString
+    deriving (Show,Eq)
+
+data XPrv = XPrv !Edwards25519.Scalar !ChainCode
+
+data XPub = XPub !Edwards25519.PointCompressed !ChainCode
+    deriving (Eq)
+
+data DerivationType = DeriveHardened | DeriveNormal
+    deriving (Show,Eq)
+
+xprv :: ByteString -> Either String XPrv
+xprv bs
+    | B.length bs /= 64 = Left ("error: xprv need to be 64 bytes: got " ++ show (B.length bs) ++ " bytes")
+    | otherwise         =
+        let (b1, b2) = B.splitAt 32 bs
+         in Right $ XPrv (Edwards25519.scalar b1) (ChainCode b2)
+
+unXPrv :: XPrv -> ByteString
+unXPrv (XPrv prv (ChainCode cc)) = B.append (Edwards25519.unScalar prv) cc
+
+xpub :: ByteString -> Either String XPub
+xpub bs
+    | B.length bs /= 64 = Left ("error: xprv need to be 64 bytes: got " ++ show (B.length bs) ++ " bytes")
+    | otherwise         =
+        let (b1, b2) = B.splitAt 32 bs
+         in Right $ XPub (Edwards25519.pointCompressed b1) (ChainCode b2)
+
+unXPub :: XPub -> ByteString
+unXPub (XPub pub (ChainCode cc)) = B.append (Edwards25519.unPointCompressed pub) cc
+
+-- | Generate extended public key from private key
+toXPub :: XPrv -> XPub
+toXPub (XPrv sec ccode) = XPub (Edwards25519.scalarToPoint sec) ccode
+
+-- | Return the Ed25519 public key associated with a XPub context
+xPubGetPublicKey :: XPub -> Ed25519.PublicKey
+xPubGetPublicKey (XPub pub _) =
+    throwCryptoError $ Ed25519.publicKey $ Edwards25519.unPointCompressed pub
+
+-- | Derive a child extended private key from an extended private key
+deriveXPrv :: XPrv -> DerivationType -> Word32 -> XPrv
+deriveXPrv (XPrv sec ccode) DeriveHardened n =
+    let (iL, iR) = walletHash $ DerivationHashHardened sec ccode n
+     in XPrv (Edwards25519.scalar iL) iR
+deriveXPrv (XPrv sec ccode) DeriveNormal n =
+    let !pub     = Edwards25519.scalarToPoint sec
+        (iL, iR) = walletHash $ DerivationHashNormal pub ccode n
+        !derived = Edwards25519.scalar iL
+     in XPrv (Edwards25519.scalarAdd sec derived) iR
+
+-- | Derive a child public from an extended public key
+deriveXPub :: XPub -> Word32 -> XPub
+deriveXPub (XPub pub ccode) n =
+    let (iL, iR) = walletHash $ DerivationHashNormal pub ccode n
+        !derived = Edwards25519.scalarToPoint $ Edwards25519.scalar iL
+     in XPub (Edwards25519.pointAdd pub derived) iR
+
+sign :: ByteArrayAccess msg => XPrv -> msg -> Ed25519.Signature
+sign (XPrv priv _) ba =
+    let sec = throwCryptoError $ Ed25519.secretKey $ Edwards25519.unScalar priv
+        pub = Ed25519.toPublic sec
+     in Ed25519.sign sec pub ba
+
+verify :: ByteArrayAccess msg => XPub -> msg -> Ed25519.Signature -> Bool
+verify (XPub point _) ba signature =
+    let pub = throwCryptoError $ Ed25519.publicKey $ Edwards25519.unPointCompressed point
+     in Ed25519.verify pub ba signature
+
+-- hashing methods either hardened or normal
+data DerivationHash where
+    DerivationHashHardened :: Edwards25519.Scalar          -> ChainCode -> Word32 -> DerivationHash
+    DerivationHashNormal   :: Edwards25519.PointCompressed -> ChainCode -> Word32 -> DerivationHash
+
+walletHash :: DerivationHash -> (ByteString, ChainCode)
+walletHash (DerivationHashHardened sec cc w) =
+    hFinalize
+            $ flip HMAC.update (encodeIndex w)
+            $ flip HMAC.update (Edwards25519.unScalar sec)
+            $ flip HMAC.update hardenedTag
+            $ hInit cc
+walletHash (DerivationHashNormal pub cc w) =
+    hFinalize
+            $ flip HMAC.update (encodeIndex w)
+            $ flip HMAC.update (Edwards25519.unPointCompressed pub)
+            $ flip HMAC.update normalTag
+            $ hInit cc
+
+hardenedTag = B.pack $ map (fromIntegral . fromEnum) "HARD"
+normalTag   = B.pack $ map (fromIntegral . fromEnum) "NORM"
+
+-- | Encode a Word32 in Big endian
+encodeIndex :: Word32 -> ByteString
+encodeIndex w = B.pack [d,c,b,a]
+  where
+    a = fromIntegral w
+    b = fromIntegral (w `div` 0xff)
+    c = fromIntegral (w `div` 0xffff)
+    d = fromIntegral (w `div` 0xffffff)
+
+hInit :: ChainCode -> HMAC.Context SHA512
+hInit (ChainCode key) = HMAC.initialize key
+
+hFinalize :: HMAC.Context SHA512 -> (ByteString, ChainCode)
+hFinalize ctx =
+    let (b1, b2) = B.splitAt 32 $ convert $ HMAC.finalize ctx
+     in (b1, ChainCode b2)
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,16 +6,18 @@ import           Test.Tasty
 import           Test.Tasty.QuickCheck
 
 import qualified Crypto.ECC.Edwards25519 as Edwards25519
+import           Cardano.Crypto.Wallet
+import qualified Data.ByteString as B
 
 data Ed = Ed Integer Edwards25519.Scalar
 
 instance Show Ed where
-    show (Ed i _) = show i
+    show (Ed i _) = "Edwards25519.Scalar " ++ show i
 instance Eq Ed where
     (Ed x _) == (Ed y _) = x == y
 instance Arbitrary Ed where
     arbitrary = do
-        (Positive n) <- arbitrary
+        (Positive n) <- arbitrary `suchThat` (\(Positive i) -> i > 2)
         return (Ed n (Edwards25519.scalarFromInteger n))
 
 testEdwards25519 =
@@ -25,7 +27,20 @@ testEdwards25519 =
     (.+) = Edwards25519.pointAdd
     ltc = Edwards25519.scalarToPoint
 
+testHdDerivation =
+    [ testProperty "pub . sec-derivation = pub-derivation . pub" normalDerive ]
+  where
+    dummyChainCode = B.replicate 32 38
+    normalDerive (Ed _ s) n =
+        let prv = either error id $ xprv (Edwards25519.unScalar s `B.append` dummyChainCode)
+            pub = toXPub prv
+            cPrv = deriveXPrv prv DeriveNormal n
+            cPub = deriveXPub pub n
+         in unXPub (toXPub cPrv) === unXPub cPub
+
+
 main :: IO ()
 main = defaultMain $ testGroup "cardano-crypto"
     [ testGroup "edwards25519-arithmetic" testEdwards25519
+    , testGroup "hd-derivation" testHdDerivation
     ]
