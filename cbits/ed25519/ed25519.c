61041f4476176dd8dc468bb237f64652713edc7e
Vincent Hanquez <vincent.hanquez@iohk.io>
2017-02-15 11:06:00 +0000

    [ed25519-variant] remove old code

--- cbits/ed25519/ed25519.c
+++ cbits/ed25519/ed25519.c
@@ -131,35 +131,5 @@ ED25519_FN(ed25519_sign_open) (const unsigned char *m, size_t mlen, const ed2551
 	return ed25519_verify(RS, checkR, 32) ? 0 : -1;
 }
 
-//#include "ed25519-donna-batchverify.h"
 
-/*
-	Fast Curve25519 basepoint scalar multiplication
-*/
-
-void
-ED25519_FN(curved25519_scalarmult_basepoint) (curved25519_key pk, const curved25519_key e) {
-	curved25519_key ec;
-	bignum256modm s;
-	bignum25519 ALIGN(16) yplusz, zminusy;
-	ge25519 ALIGN(16) p;
-	size_t i;
-
-	/* clamp */
-	for (i = 0; i < 32; i++) ec[i] = e[i];
-	ec[0] &= 248;
-	ec[31] &= 127;
-	ec[31] |= 64;
-
-	expand_raw256_modm(s, ec);
-
-	/* scalar * basepoint */
-	ge25519_scalarmult_base_niels(&p, ge25519_niels_base_multiples, s);
-
-	/* u = (y + z) / (z - y) */
-	curve25519_add(yplusz, p.y, p.z);
-	curve25519_sub(zminusy, p.z, p.y);
-	curve25519_recip(zminusy, zminusy);
-	curve25519_mul(yplusz, yplusz, zminusy);
-	curve25519_contract(pk, yplusz);
 }
--- cbits/ed25519/ed25519.h
+++ cbits/ed25519/ed25519.h
@@ -14,14 +14,6 @@ typedef unsigned char ed25519_secret_key[32];
 void cardano_crypto_ed25519_publickey(const ed25519_secret_key sk, ed25519_public_key pk);
 int cardano_crypto_ed25519_sign_open(const unsigned char *m, size_t mlen, const ed25519_public_key pk, const ed25519_signature RS);
 void cardano_crypto_ed25519_sign (const unsigned char *m, size_t mlen, const unsigned char *salt, size_t slen, const ed25519_secret_key sk, const ed25519_public_key pk, ed25519_signature RS);
-typedef unsigned char curved25519_key[32];
-
-
-//int cryptonite_ed25519_sign_open_batch(const unsigned char **m, size_t *mlen, const unsigned char **pk, const unsigned char **RS, size_t num, int *valid);
-
-//void ed25519_randombytes_unsafe(void *out, size_t count);
-
-//void curved25519_scalarmult_basepoint(curved25519_key pk, const curved25519_key e);
 
 #if defined(__cplusplus)
 }
