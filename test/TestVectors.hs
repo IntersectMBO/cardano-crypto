7d4fd1bb872fa30e66c0663ddf797da53316e8c2
Alfredo Di Napoli <alfredo.dinapoli@iohk.io>
2017-10-30 18:21:46 +0100

    Initial, tentative version of BIP-32-inspired test vectors

--- test/TestVectors.hs
+++ test/TestVectors.hs
@@ -3,10 +3,10 @@
 {-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import Control.Arrow
 import Data.String.Conv
 import Data.Bifunctor
-import Data.Maybe (fromJust)
+import Data.Monoid
+import Data.Word
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import qualified Data.Map.Strict as M
@@ -19,17 +19,26 @@ import NeatInterpolation
 
 type Path = T.Text
 
-type TestVectors = M.Map Path TestVector
-
 data TestVector = TestVector {
     seed  :: ByteString
   , pass  :: C8.ByteString
+  , path  :: Path
   }
 
 runTest :: TestVector -> (XPrv, XPub)
-runTest TestVector{..} =
-  let priv = generate (fst $ Hex.decode seed) pass
-  in (priv, toXPub priv)
+runTest TestVector{..} = case (T.splitOn "/" path) of
+  ["m"]  -> let priv = generate (fst $ Hex.decode seed) pass
+            in (priv, toXPub priv)
+  ("m":xs) -> go (generate (fst $ Hex.decode seed) pass) xs
+  where
+    go prv []     = (prv, toXPub prv)
+    go prv (x:xs) =
+      let chainCode = toChaincode x
+          prv'      = deriveXPrv pass prv chainCode
+      in go prv' xs
+
+    toChaincode :: T.Text -> Word32
+    toChaincode = read . toS . T.replace "'" mempty
 
 renderXprv :: XPrv -> T.Text
 renderXprv = toS . Hex.encode . unXPrv
@@ -37,54 +46,63 @@ renderXprv = toS . Hex.encode . unXPrv
 renderXpub :: XPub -> T.Text
 renderXpub = toS . Hex.encode . unXPub
 
-test1 :: TestVector
-test1 = TestVector {
+renderPath :: Path -> T.Text
+renderPath p = (T.intercalate "/" $ map render' (T.splitOn "/" p)) <> " (" <> p <> ")"
+  where
+    render' x
+      | "'" `T.isInfixOf` x = T.replace "'" "<sub>H</sub>" x
+      | otherwise  = x
+
+def :: TestVector
+def = TestVector {
     seed = "000102030405060708090a0b0c0d0e0f000102030405060708090a0b0c0d0e0f"
   , pass = "secret"
+  , path = "m"
   }
 
+--
+-- Test Vectors
+--
+
+testVectors :: [TestVector]
+testVectors = [
+    def
+  , def { path = "m/0'" }
+  , def { path = "m/0'/1'" }
+  , def { path = "m/0'/1'/2'" }
+  , def { path = "m/0'/1'/2'/2'" }
+  , def { path = "m/0'/1'/2'/2'/1000000000'" }
+  , noPwd
+  , noPwd { path = "m/0'" }
+  , noPwd { path = "m/0'/1'" }
+  , noPwd { path = "m/0'/1'/2'" }
+  , noPwd { path = "m/0'/1'/2'/2'" }
+  , noPwd { path = "m/0'/1'/2'/2'/1000000000'" }
+  ]
+  where
+    noPwd = def { pass = mempty }
+
 main :: IO ()
-main = do
-  -- Write the markdown file on disk.
-  T.writeFile "test-vectors.md" template
+main = T.writeFile "test-vectors.md" template
 
-
-template :: T.Text
-template =
-  let test1_path = "m"
-      test1_seed = toS $ seed test1
-      test1_pass = toS $ pass test1
-      (test1_xprv, test1_xpub) = bimap renderXprv renderXpub (runTest test1)
+testVector :: TestVector -> T.Text
+testVector tv =
+  let test_path = renderPath (path tv)
+      test_seed = toS (seed tv)
+      test_pass = let p = toS (pass tv) in if p == mempty then "(empty)" else p
+      (test_xprv, test_xpub) = bimap renderXprv renderXpub (runTest tv)
   in [text|
-==Test Vectors==
 
-===Test vector 1===
+```
+Seed (hex):  $test_seed
+Pwd (ASCII): $test_pass
+```
 
-Seed (hex):  $test1_seed
-Pwd (ASCII): $test1_pass
-
-* Chain $test1_path
-** ext pub: $test1_xpub
-** ext prv: $test1_xprv
-
-* Chain m/0<sub>H</sub>
-** ext pub: _
-** ext prv: _
-
-* Chain m/0<sub>H</sub>/1
-** ext pub: _
-** ext prv: _
-
-* Chain m/0<sub>H</sub>/1/2<sub>H</sub>
-** ext pub: _
-** ext prv: _
-
-* Chain m/0<sub>H</sub>/1/2<sub>H</sub>/2
-** ext pub: _
-** ext prv: _
-
-* Chain m/0<sub>H</sub>/1/2<sub>H</sub>/2/1000000000
-** ext pub: _
-** ext prv: _
+* *Chain* $test_path
+  * *xPub*: `$test_xpub`
+  * *xPrv*: `$test_xprv`
 
 |]
+
+template :: T.Text
+template = T.unlines (map testVector testVectors)
